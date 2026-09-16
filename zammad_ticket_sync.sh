f253f5f3049219d65cfce325e72808d4b4a3b140
Werner <werner.hause@vodafonemail.de>
2026-09-16 10:14:48 +0200

    API Script zammad_ticket_sync.sh in Version 1.1 angepasst. Ohne Klarpasswort und Benutzername. Zusätzlich ENV erstellt.

--- zammad_ticket_sync.sh
+++ zammad_ticket_sync.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+# Lade die versteckten Zugangsdaten aus der .env
+source .env
+
 ZAMMAD_URL="https://zammadwerner.skyered-devops.de/api/v1/tickets"
 OUTPUT_FILE="new_ticket_output.json"
 
 echo "Erstelle Ticket per Basic Auth..."
 
 curl -s -X POST "$ZAMMAD_URL" \
-     -u "werner.hause@skyered-education.de:WHtd200!Kg1804" \
-     -H "Content-Type: application/json" \
-     -d '{
-           "title": "Test-Ticket via Basic Auth",
-           "group_id": 1,
-           "customer": "werner.hause@skyered-education.de",
-           "article": {
-             "subject": "Test",
-             "body": "Erfolgreich via Benutzerdaten"
-           }
-         }' -o "$OUTPUT_FILE"
+  -u "$ZAMMAD_USER:$ZAMMAD_PASSWORD" \
+  -H "Content-Type: application/json" \
+  -d '{
+        "title": "Test-Ticket via Basic Auth",
+        "group_id": 1,
+        "customer": "werner.hause@skyered-education.de",
+        "article": {
+          "subject": "Test",
+          "body": "Erfolgreich via Benutzerdaten"
+        }
+      }' -o "$OUTPUT_FILE"
 
 echo "Fertig."
